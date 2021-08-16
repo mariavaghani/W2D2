ddafe413195cab6320c9632dc478b3f2f1a4ef2c
mariavaghani <maria.vaghani@gmail.com>
2021-08-16 16:20:52 -0400

    add guessing game project skeleton

--- test.rb
+++ test.rb
@@ -1,3 +1,40 @@
-puts "Enter Your Name"
-name = gets
-puts "Hello, " + name.capitalize
+# puts "Enter Your Name"
+# name = gets.chomp
+# p "Hello, " + name.capitalize
+
+# puts "Enter Your age"
+# age = gets.chomp
+# p "You are going to be " + (age.to_i + 1).to_s + " years old next year"
+
+# this class fails to encapsulate
+class Queue
+  def initialize
+    @line = []
+  end
+
+
+
+  def [](position)
+    @line[position]
+  end
+
+  def []=(position, ele)
+    @line[position] = ele
+  end
+
+  def add(ele)
+    @line << ele # add ele to back of line
+    nil
+  end
+
+  def remove
+		@line.shift  # remove front ele of line
+  end
+end
+
+grocery_checkout = Queue.new
+
+grocery_checkout.add("bob")
+grocery_checkout.add("alice")
+grocery_checkout.line.unshift("noam")   # noam cut the line!
+grocery_checkout.remove                 # => "noam"
